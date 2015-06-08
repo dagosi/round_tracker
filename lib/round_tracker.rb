39cf9ff98ff7221d050646f41998c35319c8d521
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2015-06-07 20:28:01 -0400

    Request the latest round information from the CIC website, parse the HTML with Nokogiri and create a Round object.

--- lib/round_tracker.rb
+++ lib/round_tracker.rb
@@ -1,6 +1,15 @@
 require "round_tracker/version"
 require "round_tracker/round"
 
+require "nokogiri"
+require "open-uri"
+
 module RoundTracker
-  # Your code goes here...
+  doc = Nokogiri::HTML(open("http://www.cic.gc.ca/english/express-entry/rounds.asp"))
+
+  num_invitations = doc.css('.table td').first.children.first.text
+  post_date = doc.css('section time').first.children.first.text
+  lowest_score = doc.css('.table td').last.children.first.text
+
+  puts Round.new(post_date, num_invitations, lowest_score).inspect
 end
