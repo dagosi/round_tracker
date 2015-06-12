7c2078276b731bec8a832c44281e890984d65a42
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2015-06-12 19:48:38 -0400

    Add simple email sender.

--- round_scrapper/lib/round_scrapper.rb
+++ round_scrapper/lib/round_scrapper.rb
@@ -2,5 +2,31 @@ require "round_scrapper/version"
 require "round_scrapper/round"
 require "round_scrapper/round_parser"
 
+require 'mail'
+
 module RoundScrapper
+  def self.send_email
+    options = { 
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => 'heroku.com',
+      :user_name            => ENV['MAIL_USER_NAME'],
+      :password             => ENV['MAIL_PASSWORD'],
+      :authentication       => 'plain',
+      :enable_starttls_auto => true
+    }
+
+    Mail.defaults do
+      delivery_method :smtp, options
+    end
+
+    mail = Mail.new do
+      from 'theapp@something.com'
+      to 'danielgomezsierra@gmail.com'
+      subject 'Test!'
+      body 'This is the content'
+    end
+
+    mail.deliver
+  end
 end
